caa7e695c8dda0ae14556278b6b9ac819bb7ba8f
Rodriguezbelen <beelu2.1997@gmail.com>
2024-12-04 17:33:47 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// console.log("Mi primer Js");
+// Fundamentos y Datos:
+// Ejercicio 1:
+
+// let NombreCompleto = prompt("Nombre y Apellido:");
+// console.log("Hola " + NombreCompleto);
+
+// Ejercicio 2:
+let frase = prompt("Ingresa una frase:")
+let longitud = frase.length
+console.log("La frase tiene " + longitud + " caracteres");
+let respuesta = alert("La frase tiene" + longitud + " caracteres");
+
+
